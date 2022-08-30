5b63d9830886251c8d3ea5791d65e163c56bde9e
Tan Sang Ha <tansangxtt@gmail.com>
2022-08-30 16:28:57 +0800

    RaspCam core configs and codes update

--- RaspCore/RunAToZ.sh
+++ RaspCore/RunAToZ.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>>/home/pi/raspcam.log 2>&1
+
 python3 /home/pi/Desktop/timelapse/timelapse.py
-sshpass -p $PASSWORD sftp -oBatchMode=no -oStrictHostKeyChecking=no rp1@10.8.0.1:VGD_ISD/rp1 <<< $"put -r /home/pi/Pictures/latest"
+ret=$?
+if [ $ret -ne 0 ]; then
+     echo "PiCameraError - Nothing uploaded"
+     #Handle failure
+     exit 0;
+fi
+
+chown pi:pi /home/pi/Pictures/latest
+
+sshpass -p **** sftp -oBatchMode=no -oStrictHostKeyChecking=no rp1@10.8.0.1:VGD_ISD/rp1 <<< $"put -r /home/pi/Pictures/latest"
+
 rm -rf /home/pi/Pictures/latest
+
+exit 0;
--- RaspCore/timelapse/timelapse.py
+++ RaspCore/timelapse/timelapse.py
@@ -1,8 +1,7 @@
-from picamera import PiCamera
+from picamera import PiCamera, PiCameraError, PiCameraMMALError
 from os import system
 import datetime
 from time import sleep
-#import subprocess
 
 tlminutes = 1 #set this to the number of minutes you wish to run your timelapse camera
 secondsinterval = 30 #number of seconds delay between each photo taken
@@ -13,20 +12,23 @@ print("number of photos to take = ", numphotos)
 dateraw= datetime.datetime.now()
 datetimeformat = dateraw.strftime("%Y-%m-%d_%H:%M")
 folderformat = dateraw.strftime("%Y-%m-%d-%H-%M")
-print("RPi started taking photos for your timelapse at: " + datetimeformat)
-
-camera = PiCamera()
-camera.resolution = (1024, 768)
-
-#system('rm /home/pi/Pictures/*.jpg') #delete all photos in the Pictures folder before timelapse start
-system('mkdir -p /home/pi/Pictures/latest/{}'.format(folderformat))
-
-for i in range(numphotos):
-    camera.capture('/home/pi/Pictures/latest/{}/image{}.jpg'.format(folderformat, i))
-    sleep(secondsinterval)
-print("Done taking photos.")
-print("Please standby as your timelapse video is created.")
-
+camera = -1;
+try:
+     print("RPi started taking photos for your timelapse at: " + datetimeformat)
+     system('mkdir -p /home/pi/Pictures/latest/{}'.format(folderformat))
+     camera = PiCamera()
+     camera.resolution = (1920, 1080)
+     camera.rotation = 180
+     for i in range(numphotos):
+         camera.capture('/home/pi/Pictures/latest/{}/image{}.jpg'.format(folderformat, i))
+         sleep(secondsinterval)
+     print("Done taking photos.")
+     print("Please standby as your timelapse video is created.")
+     camera.close()
+     exit(0)
+except PiCameraError:
+     print('PiCameraError exit(1)')
+     exit(1)
 # system('ffmpeg -r {} -f image2 -s 1024x768 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
 #system('rm /home/pi/Pictures/*.jpg')
 # print('Timelapse video is complete. Video saved as /home/pi/Videos/{}.mp4'.format(datetimeformat))
